ce5613a0dfe5e61fa7e93971d13c5da8422e9deb
Cleytonpsf <cleytonpsf@gmail.com>
2025-04-08 22:43:42 -0300

    fix: atualização dos middlewares das rotas de ponto de coleta

--- backend/src/routes/collectionPointRoutes.ts
+++ backend/src/routes/collectionPointRoutes.ts
@@ -7,8 +7,8 @@ const collectionPointRouter = Router();
 collectionPointRouter
     .get("/collectionPoints", AuthMiddleware.ensureAuthenticated, CollectionPointController.getAllCollectionPoint)
     .get("/collectionPoint/:id", AuthMiddleware.ensureAuthenticated, CollectionPointController.getOneCollectionPoint)
-    .post("/collectionPoint", AuthMiddleware.ensureAuthenticated, AuthMiddleware.ensureAdmin, CollectionPointController.createCollectionPoint)
-    .patch("/collectionPoint/:id", AuthMiddleware.ensureAuthenticated, AuthMiddleware.ensureAdmin, CollectionPointController.updateCollectionPoint)
-    .delete("/collectionPoint/delete/:id", AuthMiddleware.ensureAuthenticated, AuthMiddleware.ensureAdmin, CollectionPointController.deleteCollectionPoint)
+    .post("/collectionPoint", AuthMiddleware.ensureAdmin, CollectionPointController.createCollectionPoint)
+    .patch("/collectionPoint/:id", AuthMiddleware.ensureAdmin, CollectionPointController.updateCollectionPoint)
+    .delete("/collectionPoint/delete/:id", AuthMiddleware.ensureAdmin, CollectionPointController.deleteCollectionPoint)
 
 export default collectionPointRouter;
