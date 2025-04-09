a933154d2bc421d95899ba66265f22033a3458a9
Cleytonpsf <cleytonpsf@gmail.com>
2025-04-08 22:44:28 -0300

    feat(CollectionPoint): verificação dos agendamentos ao iniciar o servidor

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,6 +7,7 @@ import express from 'express';
 import cors from 'cors';
 import routes from './router'
 import DatabaseConnection from './database/connection/DatabaseConnection';
+import CollectionPointService from './services/collectionPointService';
 
 const app: express.Express = express();
 
@@ -28,7 +29,8 @@ async function startServer(): Promise<void> {
   const PORT: number = process.env.PORT ? Number(process.env.PORT) : 5000;
   const HOST: string = process.env.HOST || "0.0.0.0";
   
-  app.listen(PORT, HOST, () => {
+  app.listen(PORT, HOST, async () => {
+    await CollectionPointService.checkColectionPointNotification();
     console.log(`🔥 \tServidor ativo em: http://localhost:${PORT}`);
   });
 }
