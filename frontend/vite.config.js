23c3af4824add5d78b45ab779f31e129b44a9c6e
João Pedro Weydt de Faria <joao.faria@telecall.com>
2025-04-13 00:23:14 -0300

    fix: cleaning tests cache

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // ... other server configurations ...
-    allowedHosts: [
-      // ... other allowed hosts (if any) ...
-      'c3b3-2804-14d-5c9b-47d2-ed13-39b3-8d2-ee05.ngrok-free.app'
-    ]
-  }
+ 
   
 })
