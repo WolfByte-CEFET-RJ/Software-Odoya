1fd5a8f19b3514b583324e5788fedf37ab235a85
caiosantosdev <caiosantosdev@gmail.com>
2025-04-14 09:07:51 -0300

    feature(DepositErrors): updated DepositErrors

--- backend/src/erros/depositErrors.ts
+++ backend/src/erros/depositErrors.ts
@@ -1,13 +1,13 @@
 import { HttpCode, HttpError } from "./erro.config";
 
-/**
- * @extends HttpError
- * @description Erro de collection point não Encontrado
- */
-export class CollectionPointNotFound extends HttpError{
-    constructor(message: string 
-            = 'Ponto de coleta não Encontrado'){
-        super({status: HttpCode.NOT_FOUND, message})
+export class DepositNotFoundError extends HttpError {
+    constructor(message: string = 'Depósito não encontrado') {
+        super({ status: HttpCode.NOT_FOUND, message });
+    }
+}
 
+export class UnauthorizedDepositAccessError extends HttpError {
+    constructor(message: string = 'Você não tem permissão para acessar este depósito') {
+        super({ status: HttpCode.FORBIDDEN, message });
     }
 }
