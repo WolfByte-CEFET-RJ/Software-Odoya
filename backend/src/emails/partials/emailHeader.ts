a8fa7c7ad3fb1059e4a32f69139a88736e50e446
Cleytonpsf <cleytonpsf@gmail.com>
2025-05-18 01:35:25 -0300

    feat: header html - email

---
+++ backend/src/emails/partials/emailHeader.ts
@@ -0,0 +1,17 @@
+export const header = `
+    <!-- Header -->
+    <tr>
+        <td align="center" style="background: #269bdf; padding: 20px;">
+            <table>
+                <tr>
+                    <td>
+                        <img src="https://imgur.com/k1iO9TE.png" style="height: 3rem; vertical-align: middle;" alt="Logo">
+                    </td>
+                    <td>
+                        <img src="https://imgur.com/JOFmDtR.png" style="height: 3rem; vertical-align: middle;" alt="Nome">
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+`;
