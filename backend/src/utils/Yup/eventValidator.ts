16c798a2458dda369be3a31b400be3e775daed66
Cleytonpsf <cleytonpsf@gmail.com>
2025-05-22 16:34:34 -0300

    feat: validação e erros do Event(mutirão)

--- backend/src/utils/Yup/eventValidator.ts
+++ backend/src/utils/Yup/eventValidator.ts
@@ -1,5 +1,5 @@
 import * as Yup from 'yup';
-import { CreateEvent } from '../../types/event';
+import { CreateEvent, UpdateEvent } from '../../types/event';
 
 const regex = /^([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$/;
 
@@ -11,10 +11,21 @@ const createEventSchema = Yup.object({
     estimatedDuration: Yup.string().required('O tempo de duração é obrigatório').matches(regex, 'O horário deve estar no formato hh:mm:ss')
 });
 
+const updateEventSchema = Yup.object({
+    name: Yup.string().optional().min(3, 'O nome deve ter pelo menos 3 caracteres'),
+    location: Yup.string().optional(),
+    date: Yup.date().optional().typeError('A data precisa estar no formato ISO8601'),
+    meetingPoint: Yup.string().optional(),
+    estimatedDuration: Yup.string().optional().matches(regex, 'O horário deve estar no formato hh:mm:ss')
+});
+
 export default class EventValidator{
     
     public static async validateCreateEvent(eventData: CreateEvent){
         await createEventSchema.validate(eventData, { abortEarly: false });
     }
 
+    public static async validateUpdateEvent(eventData: UpdateEvent){
+        await updateEventSchema.validate(eventData, { abortEarly: false });
+    }
 }
