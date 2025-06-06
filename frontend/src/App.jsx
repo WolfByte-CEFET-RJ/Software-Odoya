4dff58a787f48cb8db19838f82094dac45ff8dcb
dynorph <erickmsilva19@gmail.com>
2025-06-06 14:39:52 -0300

    feat: add deposit route to app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import '../src/styles/global.scss'
 import Login from './pages/login/login.jsx'
 import Cadastro from './pages/cadastro/cadastro.jsx'
 import Principal from './pages/Principal/index.jsx'
-
+import Depositos from './pages/depositos/index.jsx'
 import Perfil from './pages/perfil/index.jsx'
 
 
@@ -18,9 +18,7 @@ const Routing = () =>{
                 <Route path="/login" element={<Login/>}/>
                 <Route path="/register" element={<Cadastro/>}/>
                 <Route path="/profile" element={<Perfil/>}/>
-
-                
-                
+                <Route path="/deposit" element={<Depositos/>}/>
 
             </Routes>
         
