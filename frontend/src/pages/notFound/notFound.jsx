e596608d11dc7c1fe75162cc009d9dd9560845d7
Gustavo  Barros <gustavoandrade0125@gmail.com>
2025-06-16 17:17:42 -0300

    style: estiliza NotFoundPage

--- frontend/src/pages/notFound/notFound.jsx
+++ frontend/src/pages/notFound/notFound.jsx
@@ -1,16 +1,22 @@
-import {React} from 'react'
+import styles from './notFound.module.scss'
+import { Link } from 'react-router-dom'
 
-function NotFoundPage(){
+function NotFound() {
+  return (
+    <div className={styles.notFoundContainer}>
+    <img
+        src="/LogoAzul.svg"
+        className={styles.logoazul}
+        alt="Logo Azul da ENACTUS"
+    />
+    <h1>404</h1>
+    <h2>Página não encontrada</h2>
+    <Link to="/">
+        <h3>Voltar para Home</h3>
+    </Link>
+    </div>
 
-    return(
-        <>
-        <h1>404</h1>
-        <h2>Página não encontrada</h2>
-        
-
-        </>
-    )
-
+  )
 }
 
-export default NotFoundPage
+export default NotFound
